8ad39be6779761ebe426eaaa4cd3738e2fef253c
Mari Ashley <mashley@oa-g1.msk.21-school.ru>
2020-07-08 14:19:44 +0300

    fdf.h

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mashley <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/08 14:12:28 by mashley           #+#    #+#             */
+/*   Updated: 2020/07/08 14:12:59 by mashley          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
 int main(int argc, char *argv[])
 {
     int fd;
@@ -8,10 +22,10 @@ int main(int argc, char *argv[])
         return (1);
     }
     fd = open(argv[1], O_RDONLY);
-    if (fd < 0 || read_file(fd, &(env.map)) != 0) //(read(fd, stack[0], 0) < 0)
-    {
-        write(2, "An error occurred reading the file... Is it valid?\n", 51);
-        return (1);
-    }
+//    if (fd < 0 || read_file(fd, &(env.map)) != 0) //(read(fd, stack[0], 0) < 0)
+//    {
+//        write(2, "An error occurred reading the file... Is it valid?\n", 51);
+//        return (1);
+//    }
     return(0);
 }
